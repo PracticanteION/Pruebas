61b19c3f9702a1b6db7c4eb219d397cb39fcdaa9
PracticanteIon <practicanteion@gmail.com>
2024-12-16 17:04:02 -0500

    avance de pruebas

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use App\Models\post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/Post', [PostController::class, 'index']);
 Route::get('/Post/Create', [PostController::class, 'create']);
 Route::get('Post/{Post}', [PostController::class, 'show']);
+Route::get('/prueba', function(){
+    $post = new Post();
+});
